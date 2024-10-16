06d53e33fc26361940d6e8facb21b66a0c7fbe4f
shawbicc <faiyazsabik99@gmail.com>
2024-10-17 00:20:47 +0600

    test file

---
+++ test_voice_gen.py
@@ -0,0 +1,16 @@
+from gtts import gTTS
+tts = gTTS('hello')
+# tts.save('hello.mp3')
+
+# import pygame
+
+# # Initialize the pygame mixer
+# pygame.mixer.init()
+
+# # Load and play the sound
+# pygame.mixer.music.load('hello.mp3')
+# pygame.mixer.music.play()
+
+# # Keep the program running until the sound finishes playing
+# while pygame.mixer.music.get_busy():
+#     pass
